783926b976b5d7db4c76a851004f3312a63d2165
alexander <alexanderhardeland@gmail.com>
2024-04-27 07:33:24 +0200

    Adds the questions in chapter three and adds the two last crack the code questions

--- src/scripts/page-scripts/chapter-3.js
+++ src/scripts/page-scripts/chapter-3.js
@@ -37,9 +37,27 @@ function taskThree() {
   console.log(dogs[myDog])
 }
 
+function taskFour() {
+  /* An attempt to access the last entry in the array is outputting undefined
+	let meanPeople = ['Layna', 'Layna again', 'Layna one more time'];
+	console.log(meanPeople[3]): */
+
+  // Indexing in an array starts from 0
+  let meanPeople = ['Layna', 'Layna again', 'Layna one more time']
+  console.log(meanPeople[2])
+}
+
+function taskFive() {
+  /* A fatal runtime error is occuring when trying to print the string
+	console.log('Layna's favorite hobby is being mean to Andrew'); */
+
+  // Wrapping text in a quote indicates a string, you need to escape the quote inside with a backslash operator
+  console.log("Layna's favorite hobby is being mean to Andrew")
+}
+
 export default function outputChapterThreePageHtml() {
   //How to get a selector from another page than the main index.html page
-  //const chapterThreeContainer = document.querySelector('#chapter-3-main-container');
-  //console.log(chapterThreeContainer);
-  // Create html to output, use the code and pre html elements to get code like output
+  //const chapterThreeQuestionsListContainer = document.querySelector('#chapter-three-question-list-container');
+  //console.log(chapterThreeQuestionsListContainer);
+  // Create html to output, use the code and pre html elements to get code like output on the page
 }

f712210f09d76b3e3b47b27b1094b27a2143fc85
alexander <alexanderhardeland@gmail.com>
2024-04-28 11:11:10 +0200

    Adds questions and tasks from chapter 5

---
+++ src/scripts/page-scripts/chapter-5.js
@@ -0,0 +1,58 @@
+function taskOne() {
+  /*
+		The loop is not printing the cat breed
+		Run the code in the terminal, Seeing is believing!
+		let catBreeds = [
+			'Main Coon',
+			'ragdoll'
+		];
+		for (let i = 0; i < catBreeds.length; i++) {
+			console.log(catBreeds[i]);
+		}
+	*/
+}
+
+function taskTwo() {
+  /*
+		The loop is not printing the bills.
+		let bills = [1, 5, 10, 20, 30];
+		for (let i = 0; i < bills.length; i++) {
+			console.log(bills[i]);
+		}
+	*/
+}
+
+function taskThree() {
+  /*
+		The loop is iterating one too many times. The last value printed is undefined
+		let hangmanLetters = ['d', 'a', 'r', 'k', 's', 'o', 'u', 'l', 's'];
+		for (let i = 0; i <= hangmanLetters.length; i++) {
+			console.log(hangmanLetters[i]);
+		}
+	*/
+}
+
+function taskFour() {
+  /*
+		The loop is not printing the cat names
+		let catNames = ['Pegasus', 'Maximillion', 'Joey'];
+		for (let i = 0; i < catNames.length; i++) {
+			console.log(catNames[i]);
+		}
+	*/
+}
+
+function taskFive() {
+  /*
+		The loop should be skipping data if the entry in the array is a string. I am trying to use
+		continue to skip the loops operations prematurely. But my loop is still printing out all of the data
+		let mixedDataTypes = ['a', 30, 'b', 'd', 'd', 1, 3];
+		for (let i = 0; i < mixedDataTypes.length; i++) {
+			let currentData = mixedDataTypes[i];
+			console.log(currentData);
+			if (typeof currentData === 'string') {
+				continue;
+			}
+		}
+	*/
+}

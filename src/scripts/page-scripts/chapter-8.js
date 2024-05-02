e4bbc7b0d353e16dd5b45d84fb67b9842023f067
Alexander <alexanderhardeland@gmail.com>
2024-05-02 11:54:45 +0000

    Adds questions and a new script file for chapter 8

---
+++ src/scripts/page-scripts/chapter-8.js
@@ -0,0 +1,6 @@
+/*
+  A rudimentary chat program is having problems running correctly. The method printlog should print
+  the chat history. This one is a little tricky, because two objects are interacting wiht each other.
+  You're using data structures in an advanced way, so don't be shy about using console.log to check the 
+  values of variables.
+*/

fb4a532623784c4c74f6893346375c949e3c1708
Alexander <alexanderhardeland@gmail.com>
2024-05-03 11:04:14 +0000

    Started adding the debugging code in chapter 7

---
+++ src/scripts/page-scripts/chapter-7.js
@@ -0,0 +1,18 @@
+/*
+  This code has a ton of problems. The function playerAboveSkillAndAge should be returing an array that includes
+  the players who are above the specified skill and age.
+
+  let players = [
+    {player: 'super', skill: 1, age: 20},
+    {player: 'smurf', skill: 8, age: 24},
+    {player: 'ANS', skill: 6, age: 27},
+    {player: 'faker', skill: 10, age: 17},
+    {player: 'shroud', skill: 9, age: 26},
+  ];
+
+  function playerAboveSkillAndAge(players, skill) {
+    let skilledPlayer = players;
+    for (let i = 0; i < players.length; i++) {
+    }
+  }
+*/

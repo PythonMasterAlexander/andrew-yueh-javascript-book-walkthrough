8feb11966d93a653c0cb1b28b0cd830df7c3222c
Alexander <alexanderhardeland@gmail.com>
2024-05-07 16:58:15 +0000

    Adds the last questions on chapter 7

--- src/scripts/page-scripts/chapter-7.js
+++ src/scripts/page-scripts/chapter-7.js
@@ -31,4 +31,27 @@
     }
     return agedPlayers; 
   }
+
+  function playersAboveSkillAndAge(players, skill, age) {
+    let skilledPlayers = playersAboveSkill(players, skill);
+    let skilledAndAgedPlayers = playersAboveAge(skilledPlayers, age);
+
+    return skilledAndAgedPlayers; 
+  }
+  console.log(playersAboveSkillAndAge(players, 6, 19));
+
+  //expected
+  [
+    {player: 'smurf', skill: 8, age: 24},
+    {player: 'shroud', skill: 9, age: 26},
+  ];
+
+  console.log(playersAboveSkillAndAge(players, 2, 16));
+  // expected
+  [
+    {player: 'smurf', skill: 8, age: 24},
+    {player: 'ANS', skill: 6, age: 27},
+    {player: 'faker', skill: 10, age: 17},
+    {player: 'shroud', skill: 9, age: 26},
+  ];
 */
